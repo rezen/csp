229d6a4f6f2f02304fd22c04392373dab95678fd
Andres Hermosilla <an2dres5m@gmail.com>
2019-06-05 18:11:18 -0700

    Fixed log location

--- csp-main/reader.php
+++ csp-main/reader.php
@@ -6,12 +6,13 @@ require 'vendor/autoload.php';
 $user_agent = $_SERVER['HTTP_USER_AGENT'];
 $doc_id     = $_GET['id'];
 $hash       = $_GET['h'];
+$log_dir    = dirname(__FILE__) . "/logs/";
 
 if (!preg_match('/^[a-z0-9]+$/', $doc_id)) {
     exit;
 }
 
-$log_file = "logs/doc-$doc_id.log";
+$log_file = "$log_dir/doc-$doc_id.log";
 if (!file_exists($log_file)) {
     exit;
 }
--- csp-main/report.php
+++ csp-main/report.php
@@ -7,6 +7,7 @@ $doc_id      = $_GET['id'];
 $data        = json_decode(file_get_contents('php://input'), true);
 $policy      = $data['csp-report']['original-policy'];
 $hash        = policyHash(str_replace($doc_id, '', $policy));
+$log_dir    = dirname(__FILE__) . "/logs/";
 
 $data['csp-report']['agent'] = $user_agent;
 $data['csp-report']['doc_id'] = $doc_id;
@@ -16,7 +17,7 @@ $as_string = json_encode($data['csp-report']);
 
 
 if (preg_match('/^[a-z0-9]+$/', $doc_id)) {
-    $log_file = "logs/doc-$doc_id.log";
+    $log_file = "$log_dir/doc-$doc_id.log";
     if (!file_exists($log_file)) {
         touch($log_file);
     }
@@ -24,10 +25,10 @@ if (preg_match('/^[a-z0-9]+$/', $doc_id)) {
     file_put_contents($log_file, "$as_string\n", FILE_APPEND);
 }
 
-if (!file_exists("logs/policy-$hash.log")) {
-    file_put_contents("logs/policy-$hash.log", json_encode(["original_policy" => $policy]));
+if (!file_exists("$log_dir/policy-$hash.log")) {
+    file_put_contents("$log_dir/policy-$hash.log", json_encode(["original_policy" => $policy]));
 }
 
-file_put_contents("logs/policy-$hash.log", "$as_string\n", FILE_APPEND);
+file_put_contents("$log_dir/policy-$hash.log", "$as_string\n", FILE_APPEND);
 
 cleanupLogs();
