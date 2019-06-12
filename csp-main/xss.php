3bdd1a078676f43fa5ec99e68ea9f2da043abd36
Andres Hermosilla <an2dres5m@gmail.com>
2019-06-12 11:23:23 -0700

    Made XSS iframe same background as parent page

--- csp-main/xss.php
+++ csp-main/xss.php
@@ -3,18 +3,19 @@
 <head>
   <title>XSS</title>
 </head>
-<body style="background:red;">
+<body style="background:rgb(39, 42, 45)">
     <form>
         <input type="password" name="password" placeholder="Password" />
         <button type="Submit">Submit</button>
     <form>
     <script>
-        var node = document.createElement("span");
-        parent.window.iframeXss = true;
-        
-        var el = parent.document.getElementById('iframe-local');
-        el.innerText = 'Changed from XSS in iframe';
-        alert("From xss.php in iframe" + Object.keys(parent));
+        (function() {
+            parent.window.iframeXss = true;
+            var el = parent.document.getElementById('iframe-local');
+            if (!el) {return;}
+            el.innerText = 'Changed from XSS in iframe';
+            alert("From xss.php in iframe" + Object.keys(parent));
+        })();
     </script>
 </body>
 </html>
