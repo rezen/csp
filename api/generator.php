bdde980eeeddb46f680cefecdf9c8647b4e9c810
Andres Hermosilla <465024+rezen@users.noreply.github.com>
2023-12-17 23:56:40 +0000

    Fix generator

--- api/generator.php
+++ api/generator.php
@@ -4,6 +4,10 @@
 header('Content-Type: application/javascript');
 header('Cache-Control: no-cache');
 
-?>
-
-alert(<?php print_r($_GET); ?>);
+$request_id = preg_replace("/[^A-Za-z0-9 ]/", '', isset($_GET['h']) ?$_GET['h'] : "_" );
+$file = "/tmp/{$request_id}_generated.js";
+if (file_exists($file)) {
+    echo file_get_contents($file);
+} else {
+    echo "console.log('Could not find $file');";
+}
