25b24bac80e58ce8b761fedebd9d8000fe8ed413
Andres Hermosilla <465024+rezen@users.noreply.github.com>
2023-12-18 01:27:40 +0000

    Fix header debug endpoint

--- api/headers.php
+++ api/headers.php
@@ -3,16 +3,10 @@
 header('Content-Type: text/plain');
 
 echo $_SERVER['HTTP_X-Vercel-Id'];
-echo "--------------------------\n";
+echo "\n-------------------------\n";
 
 foreach ($_SERVER as $key => $value) {
     if (strpos($key, 'HTTP_') === 0) {
-        $chunks = explode('_', $key);
-        $header = '';
-        for ($i = 1; $y = sizeof($chunks) - 1, $i < $y; $i++) {
-            $header .= ucfirst(strtolower($chunks[$i])).'-';
-        }
-        $header .= ucfirst(strtolower($chunks[$i])).': '.$value;
-        echo $header.'<br>';
+        echo "$key : $value\n";
     }
 }
